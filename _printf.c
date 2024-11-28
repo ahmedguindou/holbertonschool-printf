055a941b5c218a5e9b5475963db1a9bf7c51998a
ahmedguindou <guindouahmed@gmail.com>
2024-11-28 11:07:29 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -14,32 +14,5 @@ print_t _print_int[] = {
 };
 if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 return (0);
-va_start(arg, format);
-while (format[i])
-{
-if (format[i] == '%' && format[i + 1] != '%')
-{
-j = 0;
-while (_print_int[j].specifier)
-{
-if (format[i + 1] == _print_int[j].specifier[0])
-{
-len += _print_int[j].p(arg);
-i++, j = -1;
-break;
-}
-j++;
-}
-if (j != -1)
-_putchar(format[i]);
-len++;
-}
-else if (format[i] == '%' && format[i + 1] == '%')
-_putchar('%'), i++, len++;
-else
-_putchar(format[i]), len++;
-i++;
-}
-va_end(arg);
-return (len);
+
 }
