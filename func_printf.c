45d112fee243a4bce2be390141bbb04f53f42738
jdidi422 <rassiljdidi3@gmail.com>
2024-11-28 09:06:45 +0100

    i added the print_char and print_string functions

--- func_printf.c
+++ func_printf.c
@@ -1,6 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * _print_char - Prints a character from variadic arguments.
+ * @args: The list of arguments containing the character.
+ * Return: Always 1 (number of characters printed).
+ */
+int _print_char(va_list args)
+{
+char c;
+c = va_arg(arg, int);
+_putchar(c);
+return (1);
+}
+/**
+ * _print_string - Prints a string from variadic arguments.
+ * @args: The list of arguments containing the string.
+ * Return: The number of characters printed.
+ */
+int _print_string(va_list args)
+{
+unsigned int i;
+char *s;
+s = va_arg(arg, char *);
+if (s == NULL)
+s = "(null)";
+for (i = 0; s[i] != '\0'; i++)
+{
+_putchar(s[i]);
+}
+return (i);
+}
 /**
  * _print_int - Prints an integer from variadic arguments.
  * @args: The list of arguments containing the integer.
