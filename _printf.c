fe392ad11137c31dda7320428da67586b9b3ae05
ahmedguindou <guindouahmed@gmail.com>
2024-11-28 12:16:52 +0100

    printf function

--- _printf.c
+++ _printf.c
@@ -1,18 +1,43 @@
 #include "main.h"
-#include <stdarg.h>
 /**
- * _printf - Custom printf implementation
- * @format: Format string with specifiers
- * Return: Number of characters printed
- */
+* _printf - Print formatted string.
+* @format: The format string.
+* Return: Number of characters printed.
+*/
 int _printf(const char *format, ...)
 {
 va_list arg;
-unsigned int i = 0, j, len = 0;
-print_t _print_int[] = {
-{"c", _print_char}, {"s", _print_string}, {"i", _print_int}, {NULL, NULL}
+unsigned int i, j, len = 0;
+print_t print[] = {
+{"c", print_char}, {"s", print_string},
+{"i", print_i}, {"d", print_d}, {NULL, NULL}
 };
+va_start(arg, format);
 if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 return (0);
-
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%' && format[i + 1] != '%')
+{
+for (j = 0; print[j].p != NULL; j++)
+if (format[i + 1] == print[j].print[0])
+{
+len += print[j].p(arg);
+i++;
+break;
+}
+if (print[j].p == NULL)
+_putchar(format[i]);
+}
+else if (format[i] == '%' && format[i + 1] == '%')
+{
+_putchar('%');
+i++;
+}
+else
+_putchar(format[i]);
+len++;
+}
+va_end(arg);
+return (len);
 }
--- main.h
+++ main.h
@@ -1,9 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
+typedef struct print
+{
+char *print;
+int (*p)();
+} print_t;
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _print_char(va_list args);
-int _print_string(va_list args);
-int _print_int(va_list args);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
 #endif
