2be3d49e016597d6c36dc12154530abcf4d539ae
ahmedguindou <guindouahmed@gmail.com>
2024-11-28 14:10:07 +0100

    fix error of betty

--- _printf.c
+++ _printf.c
@@ -1,31 +1,30 @@
 #include "main.h"
 /**
-* _printf - print function
-* @format: character.
-* Return : printed caracter, int (success).
-*
-*/
+ * _printf - Custom printf function
+ * @format: Format string containing the characters and specifiers
+ * Description: This function produces output according to a format string
+ * and arguments. It supports the following conversion specifiers:
+ * 'c', 's', 'i', 'd', and '%%'.
+ * Return: The total number of characters printed on success,
+ * or -1 if an error occurs (e.g., NULL format or lone '%').
+ */
+#include "main.h"
 int _printf(const char *format, ...)
 {
 va_list arg;
-unsigned int i, j, flag;
-unsigned int len = 0;
+unsigned int i = 0, len = 0;
 print_t print[] = {
 {"c", print_char}, {"s", print_string},
 {"i", print_i}, {"d", print_d}, {NULL, NULL}
 };
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (-1);
 va_start(arg, format);
-if (format == NULL)
-return (-1);
-if (format[0] == '%' && format[1] == '\0')
-return (-1);
-i = 0;
 while (format[i] != '\0')
 {
 if (format[i] == '%' && format[i + 1] != '%')
 {
-j = 0;
-flag = 0;
+unsigned int j = 0, flag = 0;
 while (print[j].p != NULL)
 {
 if (format[i + 1] == print[j].print[0])
@@ -36,23 +35,14 @@ i++;
 }
 j++;
 }
-if (flag == 0)
-{
+if (!flag)
 _putchar(format[i]);
-len += 1;
-}
 }
 else if (format[i] == '%' && format[i + 1] == '%')
-{
 _putchar('%');
-i++;
-len += 1;
-}
 else
-{
 _putchar(format[i]);
 len += 1;
-}
 i++;
 }
 va_end(arg);
